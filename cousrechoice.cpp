f720423b3b4bfa8d743bba20eaa5416248cb3739
manoj.v1@s.amity.edu <manoj.v1@s.amity.edu>
2025-03-25 11:22:46 +0530

    choose ur course

---
+++ cousrechoice.cpp
@@ -0,0 +1,13 @@
+// SELECT your cousre 
+#include<stdio.h>
+ int main(){
+ char choice;
+ printf(" Enter your choice of course"):
+scanf("%c",&choice);
+ switch(choice){
+
+  case 0:
+	printf("chossen cousre b.tech CSE");
+}
+return 0;
+}
